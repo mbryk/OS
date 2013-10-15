9cb500db56cb31371483bf14a6bf54cf10258f7d
Mark Bryk <bryk@cooper.edu>
2013-10-15 19:20:13 -0400

    almost done with ps3

--- ps3/shell.c
+++ ps3/shell.c
@@ -13,49 +13,54 @@
 
 void redirection(char *file, int redir, int flags){
 	int fd = open(file, flags, 0666);
+	if(fd==-1){
+		fprintf(stderr, "Error Opening File %s for redirection: %s", file, strerror(errno));
+		exit(-1);
+	}
 	close(redir);
 	dup2(fd, redir);
 	close(fd);
 }
 
-void closeFds(){
-	int fdLimit,i;
-	if((fdLimit=getdtablesize())==-1){
-		fprintf(stderr, "Error getting max possible file descriptor: %s", strerror(errno));
-		exit(-1);
-	}
-	for(i=3; i<fdLimit; i++){
-		close(i);
-	}
-}
-
-int main(){
+int main(int argc, char **argv){
 	int n, pid, status, i, j;
-	int dup_in, dup_out, dup_err, flags_out, flags_err;
+	int fd_in, dup_in, dup_out, dup_err, flags_out, flags_err;
 	double rseconds;
 	char *file_in, *file_out, *file_err, *arg;
 	struct rusage ru;
 	struct timeval begin, end;
 
-	int b_size = 1024;
-	char *buf = malloc(b_size);
-	char *nargv[10];
+	int b_size = 4096; int arg_size = 100;
+	char *buf = malloc(b_size); char **args = malloc(arg_size);
+	if((buf==NULL)||(args==NULL)){
+		fprintf(stderr, "Error Allocating Memory: %s\n", strerror(errno));
+		exit(-1);
+	}
+
+	if(argc>1){
+		if((fd_in=open(argv[1], O_RDONLY, 0666))==-1){
+			fprintf(stderr, "Error opening Input File %s for shell to interpret: %s\n", argv[1], strerror(errno));
+			exit(-1);
+		}
+	} else fd_in = 0;
+
 	while(1){
 		dup_in = 0; dup_out = 0; dup_err = 0;
 		fprintf(stderr, "--->");
-		if((n=read(0, buf, b_size))==-1){
-			fprintf(stderr, "Error - Reading from Shell failed: %s", strerror(errno));
+		if((n=read(fd_in, buf, b_size))==-1){
+			fprintf(stderr, "Error - Reading from Shell failed: %s\n", strerror(errno));
 			exit(-1);
+		} else if(n==0) {
+			fprintf(stderr,"End of File\n");break;
 		}
-
 		buf[n-1] = '\0'; /* To get rid of new line */
-		arg = strtok(buf, " ");
 		
+		arg = strtok(buf, " ");
 		if(arg==NULL||arg[0]=='#')
 			continue;
-		nargv[0] = arg;
+		args[0] = arg;
+		
 		i=1;
-
 		while((arg = strtok(NULL, " ")) != NULL){
 			switch(arg[0]){
 				case '<':
@@ -84,20 +89,27 @@ int main(){
 					file_err = arg;					
 					break;
 				default:
-					nargv[i] = arg;
+					if(i==arg_size){
+						args = realloc(args, arg_size*10);
+						if(buf==NULL){
+							fprintf(stderr, "Error Reallocating Memory: %s\n", strerror(errno));
+							exit(-1);
+						}
+					}
+					args[i] = arg;
 					i++;
 					break;
 			}
 		}
-		nargv[i] = NULL;
+		args[i] = NULL;
 		
-		fprintf(stderr, "Executing command %s", nargv[0]);
+		fprintf(stderr, "Executing command %s", args[0]);
 		if(i!=1)
 			fprintf(stderr, " with arguments ");
-		for(j=1;nargv[j]!=NULL;j++){
-			fprintf(stderr, "\"%s\" ", nargv[j]);
+		for(j=1;args[j]!=NULL;j++){
+			fprintf(stderr, "\"%s\" ", args[j]);
 		}
-		fprintf(stderr, "\n\n");
+		fprintf(stderr, "\n");
 
 		switch(pid = fork()){
 			case -1:
@@ -106,12 +118,11 @@ int main(){
 				break;
 			case 0:
 				/* In Child */
-				closeFds();
 				if(dup_in) redirection(file_in, 0, O_RDONLY);
 				if(dup_out) redirection(file_out, 1, flags_out);
 				if(dup_err) redirection(file_err, 2, flags_err);
 
-				if((n = execvp(nargv[0], nargv))==-1){
+				if((n = execvp(args[0], args))==-1){
 					fprintf(stderr, "Error. Exec Failed: %s\n", strerror(errno));
 					exit(-1);
 				}
@@ -120,7 +131,7 @@ int main(){
 				/* In parent. ChildPID = pid */
 				gettimeofday(&begin, NULL);
 				if(wait3(&status, 0, &ru)==-1){
-					fprintf(stderr, "Error returning from child process on command %s: %s", nargv[0], strerror(errno));
+					fprintf(stderr, "Error returning from child process %d on command %s: %s", pid, args[0], strerror(errno));
 					exit(-1);
 				}
 				gettimeofday(&end, NULL);
@@ -129,8 +140,8 @@ int main(){
 		rseconds = difftime(end.tv_sec, begin.tv_sec) + difftime(end.tv_usec, begin.tv_usec)/1000000;
 		/* I use both rusage and gettimeofday since real > user + system. 
 		However, I think that part of the discrepancy comes from the time to perform the gettimeofday command!	*/
-		fprintf(stderr, "\nCommand returned with return code %d,\n", status);
+		fprintf(stderr, "\nChild [%d] returned with return code %d,\n", pid, status);
 		fprintf(stderr, "consuming %.6f real seconds, ", rseconds);
-		fprintf(stderr, "%ld.%.6d user, %ld.%.6d system\n\n", ru.ru_utime.tv_sec, ru.ru_utime.tv_usec, ru.ru_stime.tv_sec, ru.ru_stime.tv_usec);
+		fprintf(stderr, "%ld.%.6d user, %ld.%.6d system\n", ru.ru_utime.tv_sec, ru.ru_utime.tv_usec, ru.ru_stime.tv_sec, ru.ru_stime.tv_usec);
 	}
 }

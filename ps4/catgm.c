c8906edc4e0687438b774b99574b0b79370fbd45
Mark Bryk <bryk@cooper.edu>
2013-10-21 21:10:46 -0400

    im feelin 22!!!!

--- ps4/catgm.c
+++ ps4/catgm.c
@@ -7,33 +7,42 @@
 #include <errno.h>
 #include <string.h>
 #include <signal.h>
-#include <sys/signal.h>
+#include <signal.h>
+#include <sys/wait.h>
 
-int flog; char *lgbuf;
+int i;
+pid_t pid, pid2;
 
-void int_handler(int sn){
-	fprintf(stderr, "I just got a quit!");
-	lgbuf = "LQUIT";
+static void int_handler(int sn){
+	fprintf(stderr, "I just got a quit!\n");
 	exit(-1);
 }
 
-void pipe_handler(int sn){
-	fprintf(stderr, "I just got a pipe");
-	lgbuf = "LPIPE";
-}
-
-void childhandler(int sn){
-	fprintf(stderr, "I just had a baby");
-	lgbuf = "LPIPE";
+static void pipe_handler(int sn){
+	fprintf(stderr, "I just got a pipe\n");
+	kill(pid2, 9);
 }
 
 int main(int argc, char **argv){
-	int i, r, n, in_fd, status;
-	pid_t pid, pid2;
+	int r, n, in_fd, status;
 	int b_size = 4096;
+	
+	struct sigaction sai, sap;
+	sai.sa_flags = SA_SIGINFO; sap.sa_flags = SA_SIGINFO;
+	sigemptyset(&sai.sa_mask); sigemptyset(&sap.sa_mask);
+	sai.sa_sigaction = int_handler;
+	sap.sa_sigaction = pipe_handler;
+	if(sigaction(SIGINT, &sai, NULL)==-1){
+		fprintf(stderr, "Error- Could not set signal handler\n");
+		exit(-1);
+	}
+	if(sigaction(SIGPIPE, &sap, NULL)==-1){
+		fprintf(stderr, "Error- Could not set signal handler\n");
+		exit(-1);
+	}
 
 	char *grepargs[3]= {"grep", argv[1], NULL};
-	char *pgargs[2]= {"pg", NULL};
+	char *pgargs[2]= {"more", NULL};
 
 	int fds_mgrep[2], fds_gpg[2];
 	char *buf = malloc(b_size);
@@ -41,17 +50,18 @@ int main(int argc, char **argv){
 		fprintf(stderr, "Error Malloc");
 		exit(-1);
 	}
-	flog = open("lg.txt", O_WRONLY | O_CREAT | O_APPEND, 0666);
 	for(i=2;i<argc;i++){
 		pipe(fds_mgrep);
 		pipe(fds_gpg);
 		switch(pid = fork()){
 			case -1: exit(-1);break;
 			case 0:
+				if(sigaction(SIGPIPE, &sap, NULL)==-1){
+					fprintf(stderr, "Error- Could not set signal handler\n");
+					exit(-1);
+				}
 				/* In Child to do pg! */
 				/* Set up pipe read */
-
-//				signal(SIGINT, interrupt_handler);
 				dup2(fds_gpg[0], 0);
 				close(fds_gpg[0]); close(fds_gpg[1]);
 				close(fds_mgrep[0]); close(fds_mgrep[1]);
@@ -63,9 +73,12 @@ int main(int argc, char **argv){
 		switch(pid2 = fork()){
 			case -1: exit(-1); break;
 			case 0:
+				if(sigaction(SIGPIPE, &sap, NULL)==-1){
+					fprintf(stderr, "Error- Could not set signal handler\n");
+					exit(-1);
+				}
 				/* In Child to do grep! */
 				/* Set up Pipe Read From Main and Pipe Write */
-				signal(SIGPIPE, pipe_handler);
 				dup2(fds_mgrep[0], 0);
 				dup2(fds_gpg[1], 1);
 				close(fds_gpg[0]); close(fds_gpg[1]);
@@ -93,8 +106,8 @@ int main(int argc, char **argv){
 			exit(-1);
 		}
 		close(in_fd); 
-		waitpid(pid,&status); 
 		close(fds_mgrep[1]);
+		waitpid(pid2, &status, 0); waitpid(pid,&status, 0);
 	}
 	free(buf);
 	return 0;

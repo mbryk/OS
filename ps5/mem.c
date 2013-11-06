a07a042f1ffeb54c1599ed7d880a1c5d72bba922
Mark Bryk <bryk@cooper.edu>
2013-11-05 22:12:49 -0500

    cleaned up code for ps5

--- ps5/mem.c
+++ ps5/mem.c
@@ -11,10 +11,32 @@
 #include <sys/stat.h>
 #include <setjmp.h>
 
-char *filename;
+char *filename = "testfile";
 /* My Signals list is a bit different than yours. Platform specific order */
 char *signals[31] = {"SIGHUP","SIGINT","SIGQUIT","SIGILL","SIGTRAP","SIGABRT","SIGBUS","SIGFPE","SIGKILL","SIGUSR1","SIGSEGV","SIGUSR2","SIGPIPE","SIGALRM","SIGTERM","SIGSTKFLT","SIGCHLD","SIGCONT","SIGSTOP","SIGTSTP","SIGTTIN","SIGTTOU","SIGURG","SIGXCPU","SIGXFSZ","SIGVTALRM","SIGPROF","SIGWINCH","SIGIO","SIGPWR","SIGSYS"};
-void qA(); void qBC(int); void qDE(); void qF(); void EC();
+jmp_buf int_jb;
+void EC();
+
+int create_file(int size, int flags){
+	printf("Creating %d byte random file %s\n", size, filename);
+	char command[100];
+	sprintf(command, "dd if=/dev/urandom of=%s bs=%d count=1", filename, size);
+	system(command);
+	int fd = open(filename, flags);
+	return fd;
+}
+static void sig_handlerA(int sn){
+	printf("In response to question A: Signal %s is generated.\n", signals[sn-1]);
+	unlink(filename);
+	exit(0);
+}
+static void sig_handlerF(int sn){
+	printf("Uh oh!\n");
+	printf("In response to question F:\nAccessing first page results in no signal. The byte returned is a 0.\nAccessing second page results in a %s.\n", signals[sn-1]);
+	/* This happens because of demand paging. */
+	unlink(filename);
+	exit(0);
+}
 
 int main(int argc, char **argv){
 	char qc;
@@ -23,22 +45,111 @@ int main(int argc, char **argv){
 		fprintf(stderr, "Error - Improper Input.\n");
 		exit(-1);
 	}
+	int i, size, fd, offset;
+	char *addr, *str, *answer;
 	qc = argv[1][0];
+	int flag = MAP_PRIVATE;
 	switch(qc){
 		case 'A': case 'a':
-			qA();
+			for(i=1; i<32; i++){
+				if(i==2||i==9||i==16||i==17||i==19) continue;
+				if(signal(i, sig_handlerA)==SIG_ERR){
+					fprintf(stderr,"Error- Setting Signal %d Handler: %s\n", i, strerror(errno));
+				}
+			}
+			size = 4096;
+			fd = create_file(size, O_RDWR);
+			printf("About to MAP_SHARED with PROT_READ from fd %d\n", fd);
+			addr = mmap(NULL, size, PROT_READ, MAP_SHARED, fd, 0);
+			close(fd);
+			printf("About to write one byte to address %p\n", addr);
+			*addr = 'A'; 
+			printf("In response to question A: Nothing Happened!"); // Not gonna happen
+			unlink(filename);
+			exit(0);
 			break;
+
 		case 'B': case 'b':
-			qBC(MAP_SHARED);
-			break;
+			flag = MAP_SHARED;
 		case 'C': case 'c':
-			qBC(MAP_PRIVATE);
+			size = 4097;
+			fd = create_file(size, O_RDWR);
+			char *maps[2] = {"MAP_SHARED","MAP_PRIVATE"};
+			printf("About to %s with read & write from fd %d\n", maps[flag-1], fd);
+			addr = mmap(NULL, size, PROT_READ|PROT_WRITE, flag, fd, 0);
+			printf("About to write 4 bytes to offset 30 from address %p\n", addr);
+			offset = 30;
+			str = "ABCD";
+			for(i=0; i<4; i++) addr[offset+i] = str[i];
+			lseek(fd, offset, SEEK_SET);
+			char bufC[5];
+			read(fd, bufC, 4);
+			printf("Write to Memory: "); for(i=0;i<4;i++) printf("<%02X> ", str[i]);
+			printf("\nRead from File: "); for(i=0;i<4;i++) printf("<%02X> ", bufC[i]);
+			answer = strncmp(str,bufC,4)?"NO, the file is not updated with the write to memory":"YES, the update is immediately visible";
+			printf("\nIn response to question %c: %s.\n", qc, answer);
+			close(fd);
+			unlink(filename);	
 			break;
+
 		case 'D': case 'd': case 'E': case 'e':
-			qDE();
+			size = 4097;
+			fd = create_file(size, O_RDWR);
+			int filesize = 4093;
+			//	filesize = stat it!;
+			printf("The size of the file is %d bytes\n", filesize);
+			printf("About to MAP_SHARED with read & write from fd %d\n", fd);
+			addr = mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+			printf("About to write 5 bytes to offset %d\n", size);
+			str = "ABCDE";
+			for(i=0;i<5;i++) addr[size+i] = str[i];
+			//	filesize = stat it!;
+			printf("The new size of the file is %d bytes\n", filesize);
+			printf("\nMemory dump starting at offset %d:\n",size);
+			for(i=0;i<5;i++) printf("<%02X> ",addr[size+i]);
+			lseek(fd, size, SEEK_SET);
+			char bufD[20];
+			read(fd, bufD, 5);
+			printf("\nFile dump starting at offset %d:\n", size);
+			for(i=0;i<5;i++) printf("<%02X> ",bufD[i]);
+			answer = strncmp(str,bufD,5)?"NO, the file does not change":"YES, the file does change";
+			printf("\nIn response to question D: %s.\n\n", answer); // No. It does not change.
+			printf("About to expand file by 10 bytes and write 4 bytes to the end.\n");
+			char *str2 = "FGHI";
+			lseek(fd, size+10, SEEK_SET);
+			write(fd, str2, 4);
+			printf("Memory dump starting at offset %d:\n", size);
+			for(i=0;i<14;i++) printf("<%02X> ",addr[size+i]);
+			lseek(fd, size, SEEK_SET);
+			read(fd, bufD, 14);
+			printf("\nFile dump starting at offset %d:\n", size);
+			for(i=0;i<14;i++) printf("<%02X> ",bufD[i]);
+			answer = strncmp(bufD, str,5)?"NOT":"indeed";
+			printf("\nIn response to question E: The data is %s visible in the file!\n", answer);
+			close(fd);
+			unlink(filename);
 			break;
+
 		case 'F': case 'f':
-			qF();
+			for(i=1; i<32; i++){
+				if(i==2||i==9||i==16||i==17||i==19) continue;
+				if(signal(i, sig_handlerF)==SIG_ERR){
+					fprintf(stderr,"Error- Setting Signal %d Handler: %s\n", i, strerror(errno));
+				}
+			}
+			size = 12;
+			fd = create_file(size, O_RDWR);
+			printf("About to MAP_SHARED with PROT_READ from fd %d\n", fd);
+			addr = mmap(NULL, 8192, PROT_READ, MAP_SHARED, fd, 0);
+			close(fd);
+			char c;
+			offset = 30;
+			printf("About to read one byte from first page mapped... ");
+			c = addr[offset];
+			printf("Nothing Happened!\n");
+			printf("About to read one byte from second page mapped... ");	
+			c = addr[4096+offset];
+			printf("Nothing Happened?\n"); /* Not gonna happen */
 			break;
 		case 'G': case 'g': /* Extra Credit */
 			EC();
@@ -50,153 +161,10 @@ int main(int argc, char **argv){
 	}
 	return 0;
 }
-
-int create_file(int size, int flags){
-	printf("Creating %d byte random file %s\n", size, filename);
-	char command[100];
-	sprintf(command, "dd if=/dev/urandom of=%s bs=%d count=1", filename, size);
-	system(command);
-	int fd = open(filename, flags);
-	return fd;
-}
-
-static void sig_handlerA(int sn){
-	printf("In response to question A: Signal %s is generated.\n", signals[sn-1]);
-	unlink(filename);
-	exit(0);
-}
-
-void qA(){
-	int i;
-	for(i=1; i<32; i++){
-		if(i==2||i==9||i==16||i==17||i==19) continue;
-		if(signal(i, sig_handlerA)==SIG_ERR){
-			fprintf(stderr,"Error- Setting Signal %d Handler: %s\n", i, strerror(errno));
-		}
-	}
-	int size = 4096;
-	filename = "testfileA";
-	int fd = create_file(size, O_RDWR);
-
-	char *addr;
-	printf("About to MAP_SHARED with PROT_READ from fd %d\n", fd);
-	addr = mmap(NULL, size, PROT_READ, MAP_SHARED, fd, 0);
-	close(fd);
-	printf("About to write one byte to address %p\n", addr);
-	*addr = 'A'; 
-
-	/* No need to unmap or reset signal handlers, because the signal handler will kill the process and the virtual address space will be gone. */
-}
-
-void qBC(int flag){
-	int i;
-	int size = 4096;
-	filename = "testfileBC";
-	int fd = create_file(size, O_RDWR);
-	char *addr;
-	char *maps[2] = {"MAP_SHARED","MAP_PRIVATE"};
-	printf("About to %s with read & write from fd %d\n", maps[flag-1], fd);
-	addr = mmap(NULL, size, PROT_READ|PROT_WRITE, flag, fd, 0);
-	
-	printf("About to write 4 bytes to offset 30 from address %p\n", addr);
-	int offset = 30;
-	char *str = "ABCD";
-	for(i=0; i<4; i++)
-		addr[offset+i] = str[i];
-	lseek(fd, offset, SEEK_SET);
-	char buf[5];
-	read(fd, buf, 4);
-	buf[4] = '\0';
-	printf("Write to Memory: "); for(i=0;i<4;i++) printf("<%02X> ", str[i]);
-	printf("\nRead from File: "); for(i=0;i<4;i++) printf("<%02X> ", buf[i]);
-	if(flag==1) printf("\nIn response to question B: YES, the update is immediately visible.\n");
-	else printf("\nIn response to question C: NO, the file is not updated with the write to memory.\n");
-	close(fd);
-	unlink(filename);	
-}
-
-void qDE(){
-	int i;
-	int size = 4097;
-	filename = "testfileDE";
-	int fd = create_file(size, O_RDWR);
-	int filesize = 4093;
-//	filesize = stat it!;
-	printf("The size of the file is %d bytes\n", filesize);
-	char *addr;
-	printf("About to MAP_SHARED with read & write from fd %d\n", fd);
-	addr = mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
-
-	printf("About to write 5 bytes to offset %d\n", size);
-	char *str = "ABCDE";
-	for(i=0;i<5;i++)
-		addr[size+i] = str[i];
-//	filesize = stat it!;
-	printf("The new size of the file is %d bytes\n", filesize);
-	printf("\nMemory dump starting at offset %d:\n",size);
-	for(i=0;i<5;i++) printf("<%02X> ",addr[size+i]);
-	lseek(fd, size, SEEK_SET);
-	char buf[20];
-	read(fd, buf, 5);
-	buf[5] = '\0';
-	printf("\nFile dump starting at offset %d:\n", size);
-	for(i=0;i<5;i++) printf("<%02X> ",buf[i]);
-	printf("\nIn response to question D: NO, the file does not change.\n\n");
-	printf("About to expand file by 10 bytes and write 4 bytes to the end.\n");
-	str = "FGHI";
-	lseek(fd, size+10, SEEK_SET);
-	write(fd, str, 4);
-	printf("Memory dump starting at offset %d:\n", size);
-	for(i=0;i<14;i++) printf("<%02X> ",addr[size+i]);
-	lseek(fd, size, SEEK_SET);
-	read(fd, buf, 14);
-	printf("\nFile dump starting at offset %d:\n", size);
-	for(i=0;i<14;i++) printf("<%02X> ",buf[i]);
-	printf("\nIn response to question E: The data is indeed visible in the file!\n");
-	close(fd);
-	unlink(filename);
-}
-
-static void sig_handlerF(int sn){
-	printf("Uh oh!\n");
-	printf("In response to question F:\nAccessing first page results in no signal. The byte returned is a 0.\nAccessing second page results in a %s.\n", signals[sn-1]);
-	/* This happens because of demand paging. */
-	unlink(filename);
-	exit(0);
-}
-
-void qF(){
-	int i;
-	for(i=1; i<32; i++){
-		if(i==2||i==9||i==16||i==17||i==19) continue;
-		if(signal(i, sig_handlerF)==SIG_ERR){
-			fprintf(stderr,"Error- Setting Signal %d Handler: %s\n", i, strerror(errno));
-		}
-	}
-	int size = 12;
-	filename = "testfileF";
-	int fd = create_file(size, O_RDWR);
-	char *addr;
-	printf("About to MAP_SHARED with PROT_READ from fd %d\n", fd);
-	addr = mmap(NULL, 8192, PROT_READ, MAP_SHARED, fd, 0);
-	close(fd);
-	char c;
-	int offset = 30;
-	printf("About to read one byte from first page mapped... ");
-	c = addr[offset];
-	printf("Nothing Happened!\n");
-	printf("About to read one byte from second page mapped... ");
-	c = addr[4096+offset];
-	printf("Nothing Happened?\n"); /* Not gonna happen */
-}
-
-jmp_buf int_jb;
-
 static void sig_handlerEC(int sn){
 //	printf("HEY BABE %d", sn);
 	longjmp(int_jb,1);
 }
-
 void EC(){
 	int i, addr1; char c;
 	signal(7, sig_handlerEC);
@@ -211,7 +179,6 @@ void EC(){
 			} else	break;
 		}
 		c = addr[i];
-		
 	}
 	printf("%d", i);
 	printf("%d", addr1);
